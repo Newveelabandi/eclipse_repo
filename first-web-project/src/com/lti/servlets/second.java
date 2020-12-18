75f391fd76cd79308098028cf8fa9fe1ae34b9c7
Newveela <newveelabandi@gmail.com>
2020-12-18 10:13:43 +0530

    3rd commit

--- first-web-project/src/com/lti/servlets/second.java
+++ first-web-project/src/com/lti/servlets/second.java
@@ -1,11 +1,10 @@
 package com.lti.servlets;
 
-public class first {
+public class second {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1st commit");
-		System.out.println("2nd commit");
+		System.out.println(" commit");
 	}
 
 }
